d09764c25bbf6b65a99150153e794e6904445235
tahany <tahanymohammed777@hotmail.com>
2022-04-15 00:52:25 +0300

    feat: add sign up and sign in

--- src/components/Auth/Input.js
+++ src/components/Auth/Input.js
@@ -1,8 +1,32 @@
 import React from 'react'
-import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core'
-const Input = () => {
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
+import  Visibility  from '@mui/icons-material/Visibility';
+import  VisibilityOff  from '@material-ui/icons/VisibilityOff';
+
+const Input = ({ name, handleChange, label, type, half, handleShowPassword }) => {
   return (
-    <div>Input</div>
+    <Grid item xs={12} sm={half ? 6 : 12}>
+        <TextField 
+          name={name}
+          onChange={handleChange}
+          variant="outlined"
+          required
+          fullWidth
+          label={label}
+          type={type}
+          InputProps={ name === 'password' ? {
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleShowPassword}>
+                  {type === "password" ? <Visibility /> : <VisibilityOff />}
+
+                </IconButton>
+              </InputAdornment>
+            )
+          } : null}
+        />
+
+    </Grid>
   )
 }
 
