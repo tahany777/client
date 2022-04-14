edcdc7ad52b586c59bbc5911beed62dc7c37e536
tahany <tahanymohammed777@hotmail.com>
2022-04-14 23:55:12 +0300

    feat: first structure for Auth

---
+++ src/components/Auth/Input.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core'
+const Input = () => {
+  return (
+    <div>Input</div>
+  )
+}
+
+export default Input
