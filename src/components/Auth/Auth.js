95a3b69d7f82ece0731239a5dda42638c80ac859
tahany <tahanymohammed777@hotmail.com>
2022-04-16 23:22:09 +0300

    feat: complete the form for sign up and sign in

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Avatar, Button, Paper, Grid, Typography, Container} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
@@ -6,7 +6,7 @@ import Input from './Input';
 const Auth = () => {
   const classes = useStyles();
   const [showPassword, setShowPassword] = useState(false);
-  const isSignup = false;
+  const [isSignup, setIsSignup ]= useState(false);
   const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
   const handleSubmit = () => {
 
@@ -14,6 +14,10 @@ const Auth = () => {
   const handleChange = () => {
 
   };
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    handleShowPassword(false);
+  }
   return (
     <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
@@ -38,6 +42,13 @@ const Auth = () => {
           <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
             {isSignup ? 'Sign Up' : 'Sign In'}
           </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignup ? 'Already have an account? Sign In' : "Don't have an account? Sign Up"}
+              </Button>
+            </Grid>
+          </Grid>
         </form>
       </Paper>
     </Container>
